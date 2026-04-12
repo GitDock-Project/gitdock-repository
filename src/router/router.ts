f3ba9e3196a663c2f9995e1854534fed188c65b6
Maryam Loukili <maryamloukili78@gmail.com>
2026-04-12 12:36:59 +0100

    recuperation des fichiers vue supprimés

--- src/router/router.ts
+++ src/router/router.ts
@@ -1,10 +1,29 @@
 import { createRouter, createWebHistory } from "vue-router";
 import type { RouteRecordRaw } from "vue-router";
+
 import DashboardTask from "../views/DashboardTask.vue";
+import AddTask from "../views/AddTask.vue";
+import UpdateTask from "../views/UpdateTask.vue";
 
 const routes: Array<RouteRecordRaw> = [
-  { path: "/", redirect: "/dashboardtask" }, // redirection vers la seule page
-  { path: "/dashboardtask", component: DashboardTask }
+  { path: "/", redirect: "/dashboardtask" },
+
+  { path: "/dashboardtask", component: DashboardTask },
+
+  { path: "/addtask", component: AddTask },
+
+  {
+    path: "/tasks/edit/:id",
+    name: "EditTask",
+    component: UpdateTask
+  },
+
+  // Nouvelle route pour DeleteTask
+  {
+    path: "/tasks/delete/:id",
+    name: "DeleteTask",
+    component: () => import("../views/DeleteTask.vue")
+  }
 ];
 
 export const router = createRouter({
